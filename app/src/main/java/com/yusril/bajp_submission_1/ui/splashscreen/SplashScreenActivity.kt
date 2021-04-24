c16b997c60f7a1df8040934ba8c611a0f9d92e43
yysrll <yusril.uzumaki@gmail.com>
2021-04-24 12:35:26 +0800

    added splash screen

---
+++ app/src/main/java/com/yusril/bajp_submission_1/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,32 @@
+package com.yusril.bajp_submission_1.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.bumptech.glide.Glide
+import com.yusril.bajp_submission_1.R
+import com.yusril.bajp_submission_1.databinding.ActivitySplashScreenBinding
+import com.yusril.bajp_submission_1.ui.home.HomeActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Glide.with(this)
+            .load(R.drawable.icon)
+            .into(binding.logo)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
